892baec13086b5eb159bc633c26bfacbe34762fd
pamy1q <britneyojos@gmail.com>
2024-09-29 17:53:21 +0100

    bitwise

---
+++ bitwiseoperator.py
@@ -0,0 +1,5 @@
+a=10
+b=20
+print(a>>1)#bitwise right
+print(a<<1)#bitwise left
+print(b<<1)
---
+++ identityoperator.py
@@ -0,0 +1,13 @@
+x=4.5
+print(type(x))
+if type(x) is int:
+    print("True")
+else:
+    print("False")
+print("identitycheck")#identity
+a=50
+b=50
+if a is b:
+    print("tRUE")
+else:
+    print("False")
